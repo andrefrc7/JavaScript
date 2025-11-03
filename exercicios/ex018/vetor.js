37e97cac27e8b700bdc27dca1557c6479e9f8941
André França <andreperema@gmail.com>
2025-11-03 11:58:20 -0300

    ex018

---
+++ exercicios/ex018/vetor.js
@@ -0,0 +1,11 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+/*
+for(let pos = 0; pos < num.length; pos++) {
+    console.log(`A posicao ${pos} tem o valor ${valores[pos]}`)
+}
+*/
+
+for(let pos in num) {
+    console.log(`A posicao ${pos} tem o valor ${valores[pos]}`)
+}
